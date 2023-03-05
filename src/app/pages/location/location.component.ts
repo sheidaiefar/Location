78afdb4971925eb8a4dd4fb5d20e5ed87c980e16
=Malihe Sheidaiefar <m.sheidaiefar@rayanbourse.ir>
2023-03-05 18:41:41 +0330

    location form

--- src/app/pages/location/location.component.ts
+++ src/app/pages/location/location.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
 import * as L from 'leaflet';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {LocationModel} from "../models/location.model";
 
 @Component({
   selector: 'app-location',
@@ -8,7 +9,27 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./location.component.css']
 })
 export class LocationComponent {
+  location!: LocationModel;
   shareLocationForm!: FormGroup;
+  public data: any;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    if (this.data.formData) {
+      //edit mode
+      this.shareLocationForm = new FormGroup({
+        'name': new FormControl(this.data.formData.name, Validators.required),
+
+      })
+    } else {
+      this.shareLocationForm = new FormGroup({
+        'name': new FormControl(null, Validators.required),
+
+      })
+    }
+  }
 
   onSubmitForm() {
   }

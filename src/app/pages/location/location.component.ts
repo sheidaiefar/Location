361dd866a3e0d16eeb60646bbb91c7ca2ebfc271
=Malihe Sheidaiefar <m.sheidaiefar@rayanbourse.ir>
2023-03-06 08:35:24 +0330

    formControl

--- src/app/pages/location/location.component.ts
+++ src/app/pages/location/location.component.ts
@@ -21,42 +21,56 @@ export class LocationComponent {
   }
 
   ngOnInit(): void {
-    if (this.location) {
-      //edit mode
-      this.shareLocationForm = new FormGroup({
-        'name': new FormControl(this.location.name, Validators.required),
 
-      })
-    } else {
-      this.shareLocationForm = new FormGroup({
-        'name': new FormControl(null, Validators.required),
+    this.shareLocationForm = new FormGroup<any>(
+      {
+        'name': new FormControl(null),
+        'type': new FormControl(null),
+        'coordinates': new FormControl([0, 0])
+      });
+    this.shareLocationForm.valueChanges.subscribe(
+      (value)=>console.log(value)
+   );
 
-      })
-    }
+    // if (this.location) {
+    //   //edit mode
+    //   this.shareLocationForm = new FormGroup({
+    //     'name': new FormControl(this.location.name, Validators.required),
+    //
+    //   })
+    // } else {
+    //   this.shareLocationForm = new FormGroup({
+    //     'name': new FormControl(null, Validators.required),
+    //
+    //   })
+    // }
 
 
   }
 
   onSubmitForm() {
-      location = {
-      name: this.shareLocationForm.value.name,
-    }
-    if (this.data.formData) {
-      //edit mode
-      data.id = this.data.formData.id;
-      this.subscription.add(this.budgetPeriodCrudService.update(data).subscribe(res => {
-        if (res) {
-          // this.dialogRef.close({status: 'success-update', res: res});
-        }
-      }));
-    } else {
-      this.subscription.add(this.budgetPeriodCrudService.create(data).subscribe(res => {
-        if (res) {
-          // this.dialogRef.close({status: 'success-add', res: res});
-        }
-      }));
-    }
+    console.log(this.shareLocationForm)
+
+    //   location = {
+    //   name: this.shareLocationForm.value.name,
+    // }
+    // if (this.data.formData) {
+    //   //edit mode
+    //   data.id = this.data.formData.id;
+    //   this.subscription.add(this.budgetPeriodCrudService.update(data).subscribe(res => {
+    //     if (res) {
+    //       // this.dialogRef.close({status: 'success-update', res: res});
+    //     }
+    //   }));
+    // } else {
+    //   this.subscription.add(this.budgetPeriodCrudService.create(data).subscribe(res => {
+    //     if (res) {
+    //       // this.dialogRef.close({status: 'success-add', res: res});
+    //     }
+    //   }));
+    // }
   }
+
 
   close() {
   }
